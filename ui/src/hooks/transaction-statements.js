5eb90a53a4dd131a8792814fc0e63242e7d55b80
Oscar Alejandro López Ramos <olopez@samcart.com>
2023-06-26 09:12:53 -0600

    Update transactions table on create

--- ui/src/hooks/transaction-statements.js
+++ ui/src/hooks/transaction-statements.js
@@ -1,15 +1,17 @@
 import useSWR from 'swr';
 import axios from 'axios';
 
-const fetcher = (url) => axios.get(url).then((res) => res.data);
+const fetcher = (accountId, continuationToken, pageSize) =>
+  axios
+    .get(
+      `${process.env.NEXT_PUBLIC_API_URL}/statement/${accountId}/?pageSize=${pageSize}${
+        continuationToken && `&continuationToken=${continuationToken}`
+      }`
+    )
+    .then((res) => res.data);
 
 const useTransactionsStatement = (accountId, continuationToken = null, pageSize = 10) => {
-  return useSWR(
-    `${process.env.NEXT_PUBLIC_API_URL}/statement/${accountId}/?pageSize=${pageSize}${
-      continuationToken && `&continuationToken=${continuationToken}`
-    }`,
-    fetcher
-  );
+  return useSWR('transactions', () => fetcher(accountId, continuationToken, pageSize));
 };
 
 export default useTransactionsStatement;
