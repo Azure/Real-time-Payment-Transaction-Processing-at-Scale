21cce3d768de18556517dac7147e4a0d07f04e6c
Oscar Alejandro López Ramos <olopez@samcart.com>
2023-06-26 09:23:04 -0600

    Update account grid on creation

--- ui/src/hooks/accounts.js
+++ ui/src/hooks/accounts.js
@@ -1,15 +1,16 @@
 import useSWR from 'swr';
 import axios from 'axios';
 
-const fetcher = (url) => axios.get(url).then((res) => res.data);
+const fetcher = (continuationToken, pageSize) =>
+  axios
+    .get(
+      `${process.env.NEXT_PUBLIC_API_URL}/accounts?pageSize=${pageSize}${
+        continuationToken && `&continuationToken=${continuationToken}`
+      }`
+    )
+    .then((res) => res.data);
 
-const useAccounts = (continuationToken = null, pageSize = 10) => {
-  return useSWR(
-    `${process.env.NEXT_PUBLIC_API_URL}/accounts?pageSize=${pageSize}${
-      continuationToken && `&continuationToken=${continuationToken}`
-    }`,
-    fetcher
-  );
-};
+const useAccounts = (continuationToken = null, pageSize = 10) =>
+  useSWR('accounts', () => fetcher(continuationToken, pageSize));
 
 export default useAccounts;
