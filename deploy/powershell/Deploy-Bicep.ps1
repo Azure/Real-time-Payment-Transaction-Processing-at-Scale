d8001a479bdc9086a72ed76ffe4a1d03cc55b4f4
Marcelo Fonseca <marcrod@microsoft.com>
2023-07-19 16:04:59 -0300

    fix deployment

--- deploy/powershell/Deploy-Bicep.ps1
+++ deploy/powershell/Deploy-Bicep.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [parameter(Mandatory=$true)][string]$resourceGroup,
-    [parameter(Mandatory=$true)][string]$location,
+    [parameter(Mandatory=$true)][string]$locations,
     [parameter(Mandatory=$false)][string]$template="main.bicep",
     [parameter(Mandatory=$false)][string]$openAiName,
     [parameter(Mandatory=$false)][string]$openAiRg,
@@ -19,14 +19,16 @@ Write-Host "--------------------------------------------------------" -Foregroun
 Write-Host "Deploying Bicep script $script" -ForegroundColor Yellow
 Write-Host "-------------------------------------------------------- " -ForegroundColor Yellow
 
+$firstLocation = $locations.Split(',')[0]
+
 $rg = $(az group show -n $resourceGroup -o json | ConvertFrom-Json)
 if (-not $rg) {
-    Write-Host "Creating resource group $resourceGroup in $location" -ForegroundColor Yellow
-    az group create -n $resourceGroup -l $location
+    Write-Host "Creating resource group $resourceGroup in $firstLocation" -ForegroundColor Yellow
+    az group create -n $resourceGroup -l $firstLocation
 }
 
 Write-Host "Beginning the Bicep deployment..." -ForegroundColor Yellow
 Push-Location $sourceFolder
-$deploymentState = $(az deployment group create -g $resourceGroup --template-file $script --parameters suffix=$suffix  --parameters openAiName=$openAiName --parameters openAiDeployment=$openAiDeployment --parameters openAiResourceGroup=$openAiRg --query "properties.provisioningState" -o tsv)
+$deploymentState = $(az deployment group create -g $resourceGroup --template-file $script --parameters suffix=$suffix --parameters locations=$locations --parameters openAiName=$openAiName --parameters openAiDeployment=$openAiDeployment --parameters openAiResourceGroup=$openAiRg --query "properties.provisioningState" -o tsv)
 Pop-Location
 Pop-Location
